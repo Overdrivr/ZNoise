473392ce2812b3d75052cf2ca8c8db1c7577b6b9
Rémi Bèges <remi.beges@gmail.com>
2015-08-07 17:38:09 +0200

    Updated generate-all test
    Added worley
    + removed compilation warnings

--- tests/generate_all_noises/main.cpp
+++ tests/generate_all_noises/main.cpp
@@ -2,20 +2,24 @@
 #include "../../examples/common/CImg.h"
 #include "../../include/Perlin2D.hpp"
 #include "../../include/Simplex2D.hpp"
+#include "../../include/Worley2D.hpp"
 
 using namespace std;
 using namespace cimg_library;
 
 void generate_perlin(CImg<unsigned char> & image);
 void generate_simplex(CImg<unsigned char> & image);
+void generate_worley(CImg<unsigned char> & image,WorleyFunction worleyFunc);
 
 // You need to add libgdi32.a in your IDE linker options
 
 int main()
+
 {
     CImg<unsigned char> image(128,128,1,3,0);
     generate_perlin(image);
     generate_simplex(image);
+    generate_worley(image,WorleyFunction_F1);
 
     return 0;
 }
@@ -26,13 +30,13 @@ void generate_perlin(CImg<unsigned char> & image)
     Perlin2D perlin2d;
     perlin2d.Shuffle(10);
 
-    for(unsigned int x = 0 ; x < image.width() ; x++)
-        for(unsigned int y = 0 ; y < image.height() ; y++)
+    for(int x = 0 ; x < image.width() ; x++)
+        for(int y = 0 ; y < image.height() ; y++)
         {
             perlin2d.Set(x,y);
             float value =  perlin2d.Get();
 
-            unsigned int greyscale = static_cast<int>((value + 1.f) / 2.f * 255.f);
+            unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
 
             image(x,y,0) = greyscale;
             image(x,y,1) = greyscale;
@@ -40,7 +44,7 @@ void generate_perlin(CImg<unsigned char> & image)
         }
 
     image.save("perlin2d.bmp");
-    std::cout<<"Perlin2D generated. Saved to perlin2d.bmp"<<std::endl;
+    std::cout<<"Saved perlin2d.bmp"<<std::endl;
 }
 
 void generate_simplex(CImg<unsigned char> & image)
@@ -49,13 +53,13 @@ void generate_simplex(CImg<unsigned char> & image)
     Simplex2D simplex2d;
     simplex2d.Shuffle(10);
 
-    for(unsigned int x = 0 ; x < image.width() ; x++)
-        for(unsigned int y = 0 ; y < image.height() ; y++)
+    for(int x = 0 ; x < image.width() ; x++)
+        for(int y = 0 ; y < image.height() ; y++)
         {
             simplex2d.Set(x,y);
             float value =  simplex2d.Get();
 
-            unsigned int greyscale = static_cast<int>((value + 1.f) / 2.f * 255.f);
+            unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
 
             image(x,y,0) = greyscale;
             image(x,y,1) = greyscale;
@@ -63,5 +67,29 @@ void generate_simplex(CImg<unsigned char> & image)
         }
 
     image.save("simplex2d.bmp");
-    std::cout<<"Simplex2D generated. Saved to simplex2d.bmp"<<std::endl;
+    std::cout<<"Saved simplex2d.bmp"<<std::endl;
+}
+
+void generate_worley(CImg<unsigned char> & image,WorleyFunction worleyFunc)
+{
+    /* Generate Simplex 2D */
+    Worley2D worley2d(worleyFunc);
+    worley2d.Shuffle(10);
+
+    for(int x = 0 ; x < image.width() ; x++)
+        for(int y = 0 ; y < image.height() ; y++)
+        {
+            worley2d.Set(x,y);
+            //std::cout<<"ok";
+            float value =  worley2d.Get();
+
+            unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
+
+            image(x,y,0) = greyscale;
+            image(x,y,1) = greyscale;
+            image(x,y,2) = greyscale;
+        }
+
+    image.save("worley2d.bmp");
+    std::cout<<"Saved worley2d.bmp"<<std::endl;
 }
