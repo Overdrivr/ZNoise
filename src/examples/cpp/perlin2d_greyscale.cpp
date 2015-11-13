a534747fcf0cd05218d4b74f79b991c0d1b49497
Rémi Bèges <remi.beges@gmail.com>
2015-11-13 16:33:04 +0100

    updated perlin example

--- src/examples/cpp/perlin2d_greyscale.cpp
+++ src/examples/cpp/perlin2d_greyscale.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-#include "Perlin2D.hpp"
+#include "Perlin.hpp"
 #include "include_all.hpp"
 
 void generate_perlin(cimg_library::CImg<unsigned char> & image)
 {
     /* Generate Perlin 2D */
-    Perlin2D perlin2d;
+    Perlin perlin2d;
     perlin2d.Shuffle(10);
 
     for(int x = 0 ; x < image.width() ; x++)
         for(int y = 0 ; y < image.height() ; y++)
         {
-            perlin2d.Set(x,y);
-            float value =  perlin2d.Get();
+
+            float value =  perlin2d.Get({x , y},0.01f);
 
             unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
 
@@ -23,4 +23,50 @@ void generate_perlin(cimg_library::CImg<unsigned char> & image)
 
     image.save("perlin2d.bmp");
     std::cout<<"Saved perlin2d.bmp"<<std::endl;
+}
+
+void generate_perlin_3dslice(cimg_library::CImg<unsigned char> & image)
+{
+    /* Generate Perlin 3D */
+    Perlin perlin3d;
+    perlin3d.Shuffle(10);
+
+    for(int x = 0 ; x < image.width() ; x++)
+        for(int y = 0 ; y < image.height() ; y++)
+        {
+
+            float value =  perlin3d.Get({x , y, 0.f},0.01f);
+
+            unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
+
+            image(x,y,0) = greyscale;
+            image(x,y,1) = greyscale;
+            image(x,y,2) = greyscale;
+        }
+
+    image.save("perlin3d.bmp");
+    std::cout<<"Saved perlin3d.bmp"<<std::endl;
+}
+
+void generate_perlin_4dslice(cimg_library::CImg<unsigned char> & image)
+{
+    /* Generate Perlin 4D */
+    Perlin perlin4d;
+    perlin4d.Shuffle(10);
+
+    for(int x = 0 ; x < image.width() ; x++)
+        for(int y = 0 ; y < image.height() ; y++)
+        {
+
+            float value =  perlin4d.Get({x , y, 0.f, 1.f},0.01f);
+
+            unsigned int greyscale = static_cast<unsigned int>((value + 1.f) / 2.f * 255.f);
+
+            image(x,y,0) = greyscale;
+            image(x,y,1) = greyscale;
+            image(x,y,2) = greyscale;
+        }
+
+    image.save("perlin4d.bmp");
+    std::cout<<"Saved perlin4d.bmp"<<std::endl;
 }
