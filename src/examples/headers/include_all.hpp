a055f548dbcf15ea2d80122b9757c1d05986ce57
Rémi Bèges <remi.beges@gmail.com>
2015-11-13 16:33:27 +0100

    updated perlin example header

--- src/examples/headers/include_all.hpp
+++ src/examples/headers/include_all.hpp
@@ -1,6 +1,13 @@
+#ifndef INCLUDE_ALL_EXAMPLES_HPP
+#define INCLUDE_ALL_EXAMPLES_HPP
+
 #include "CImg.h"
 #include "Enums.hpp"
 
 void generate_perlin(cimg_library::CImg<unsigned char> & image);
+void generate_perlin_3dslice(cimg_library::CImg<unsigned char> & image);
+void generate_perlin_4dslice(cimg_library::CImg<unsigned char> & image);
 void generate_simplex(cimg_library::CImg<unsigned char> & image);
 void generate_worley_F1(cimg_library::CImg<unsigned char> & image,WorleyFunction worleyFunc);
+
+#endif
