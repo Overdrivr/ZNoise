a50b4947b881503b5515b283965483e856ec44bf
Rémi Bèges <remi.beges@gmail.com>
2015-11-14 17:13:47 +0100

    updated main.cpp

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include "include_all.hpp"
 
-
 int main()
 {
-    cimg_library::CImg<unsigned char> output(128,128,1,3,0);
+    cimg_library::CImg<unsigned char> output(512,512,1,3,0);
 
     // Example 0 - See scr/examples/cpp/perlin2d_greyscale.cpp
     generate_perlin(output);
+    generate_perlin_3dslice(output);
+    generate_perlin_4dslice(output);
 
     // Example 1 - See scr/examples/cpp/simplex2d_greyscale.cpp
     generate_simplex(output);
+    generate_simplex_3dslice(output);
+    generate_simplex_4dslice(output);
 
     // Example 2 - See scr/examples/cpp/simplex2d_greyscale.cpp
-    generate_worley_F1(output,WorleyFunction_F1);
+    generate_worley(output,WorleyFunction_F1);
     return 0;
 }
