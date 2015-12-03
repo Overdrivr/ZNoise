13db5daf49c59008560da417a88bb49c30cef6c4
Rémi Bèges <remi.beges@gmail.com>
2015-12-03 15:09:06 +0100

    Added async example

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -6,23 +6,26 @@ int main()
     cimg_library::CImg<unsigned char> output(512,512,1,3,0);
 
     // Example 0 - See scr/examples/cpp/example-perlin.cpp
-    generate_perlin(output);
-    generate_perlin_3dslice(output);
-    generate_perlin_4dslice(output);
+    //generate_perlin(output);
+    //generate_perlin_3dslice(output);
+    //generate_perlin_4dslice(output);
 
     // Example 1 - See scr/examples/cpp/example-simplex.cpp
-    generate_simplex(output);
-    generate_simplex_3dslice(output);
-    generate_simplex_4dslice(output);
+    //generate_simplex(output);
+    //generate_simplex_3dslice(output);
+    //generate_simplex_4dslice(output);
 
     // Example 2 - See scr/examples/cpp/example-worley.cpp
-    generate_worley(output,WorleyFunction_F1);
+    //generate_worley(output,WorleyFunction_F1);
 
     // Example 3 - See scr/examples/cpp/example-fbm.cpp
-    generate_fbm(output);
+    //generate_fbm(output);
 
     // Example 4 - See scr/examples/cpp/example-fbm.cpp
-    generate_hmf(output);
-    
+    //generate_hmf(output);
+
+    // Example 5 - use std::async to parallelize densities computations
+    benchmark_async();
+
     return 0;
 }
